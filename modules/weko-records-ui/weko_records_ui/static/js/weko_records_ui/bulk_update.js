25275ad20d44dc565f7a800482bf02a4f21109f7
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-22 19:45:39 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -17,32 +17,30 @@ require([
       }else {
         $(accessDate).attr('disabled', 'disabled');
       }
-
-
     });
 
     $('select[name="field-select"]').change(function() {
       var selected = $(this).val();
-      alert($('input[name="access_type"]:checked').val());
+//      alert($('input[name="access_type"]:checked').val());
 
-//      if('3' === selected.toString()){
-//        content = $(this).parents('.field-row').find('.field-content.licence-select');
-//        $(content).removeAttr("hidden");
-//      }
-//      contents.each(function(i, elem) {
-//          if('3' === selected.toString()){
-//            $('select[name="licence-select"]')
+      if('1' === selected.toString()){
+        contents = $(this).parents('.field-row').find('.field-content');
+        contents.each(function(i, elem) {
+          alert($(elem).attr('class'));
+//            if('3' === selected.toString()){
+//              $('select[name="licence-select"]')
 //
-//          }
+//            }
 //
-//          if('4' === selected.toString()){
-//            $(elem).removeAttr("hidden");
-//          }else if($(elem).attr('hidden') == undefined) {
-//            $(elem).attr('hidden', 'hidden');
+//            if('4' === selected.toString()){
+//              $(elem).removeAttr("hidden");
+//            }else if($(elem).attr('hidden') == undefined) {
+//              $(elem).attr('hidden', 'hidden');
 //
-//          }
-//
-//      });
+//            }
+
+        });
+      }
 
 
     });
