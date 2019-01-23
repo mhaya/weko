6fd17a1170e5492a208c47cd65a64b1902af5c60
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-23 15:07:55 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -4,6 +4,7 @@ require([
 ], function() {
 
     $(document).ready(function() {
+      alert('Bulk Ready!!!!');
       newField = $('.field-row-default').first().clone(true)
       $(newField).attr('class', 'row field-row');
       $(newField).removeAttr("hidden");
