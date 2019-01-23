45cd1aaebad46e531f847049eec4fb73b109c967
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-23 10:47:16 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -3,6 +3,15 @@ require([
   "bootstrap"
 ], function() {
 
+    $(document).ready(function() {
+      newField = $('.field-row-default').first().clone(true)
+      $(newField).attr('class', 'row field-row');
+      $(newField).removeAttr("hidden");
+
+      $(newField).insertBefore($('#add-link-row'));
+
+    });
+
     // Add field
     $('#add-field-link').on('click', function(){
       newField = $('.field-row-default').first().clone(true)
