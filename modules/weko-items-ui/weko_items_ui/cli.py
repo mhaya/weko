302e66e1a2805f9f9d88798a83b5c9b3509b4aa6
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-25 16:36:53 +0900

    add cli

---
+++ modules/weko-items-ui/weko_items_ui/cli.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of WEKO3.
+# Copyright (C) 2017 National Institute of Informatics.
+#
+# WEKO3 is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# WEKO3 is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WEKO3; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""WEKO3 module docstring."""
+
+import click
+import datetime
+import uuid
+from flask.cli import with_appcontext
+from sqlalchemy import asc
+from invenio_db import db
+
+
+@click.group()
+def SHERPA():
+    """
+    Get SHERPA/ReMOO data !!
+    :return:
+    """
+
+
+@SHERPA.command('init')
+@with_appcontext
+def init_s_r_():
+    click.secho('Just do test!!!!!', fg='red')
