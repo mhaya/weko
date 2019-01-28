56bd9ee3f51e8a679e39c3d2a11a41bdf4738150
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-28 15:29:55 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -142,19 +142,19 @@ require([
           itemsMeta = data;
           Object.keys(itemsMeta).forEach(function(key) {
             itemsMeta[key].meta.lang = 'ja';
-            alert(JSON.stringify(itemsMeta[key].meta));
+//            alert(JSON.stringify(itemsMeta[key].meta));
 
-//            meta = JSON.stringify(itemsMeta[key].meta);
-//            index = JSON.stringify(itemsMeta[key].index);
-//
-//            index_url = redirect_url + "/" + key;
-//            self_url = items_url + "/" + key;
-//
-//            // Update items
-//            updateItems(index_url,
-//                        self_url,
-//                        meta,
-//                        index);
+            meta = JSON.stringify(itemsMeta[key].meta);
+            index = JSON.stringify(itemsMeta[key].index);
+
+            index_url = redirect_url + "/" + key;
+            self_url = items_url + "/" + key;
+
+            // Update items
+            updateItems(index_url,
+                        self_url,
+                        meta,
+                        index);
 
           // Get bucket
 //          $.ajax({
