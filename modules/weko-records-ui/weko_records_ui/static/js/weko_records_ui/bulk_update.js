fc6c1e1eca635951062d31809da52b2d98fe2421
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-28 13:42:09 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -118,7 +118,7 @@ require([
         url: '/bulk_update/items_metadata',
         async: false,
         success: function(data, status){
-          alert(data['4']);
+          alert(JSON.stringify(data));
 //          properties_obj = data;
 //
 //          defProps = data.defaults;
