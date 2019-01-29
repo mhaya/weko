a866b45c67f2fd01600a18ca74825baa3a7dff38
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-29 19:18:05 +0900

    Add contents meta upload function.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -49,7 +49,7 @@ require([
       // Get selected fields
       var isDuplicate = false;
 
-      $('.row.field-row').each(function(i, row) {
+      $(this).parents('.field-row').next('.field-row').each(function(i, row) {
 
         var field = $($(row).find('select[name="field_sel"]')[0]);
         alert(field.prop('value'));
