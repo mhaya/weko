83cd27e54a1606f1b1337cc1e2d7a73dffa4bca7
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-31 14:26:11 +0900

    Add bulk update function for item's metadata.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/bulk_update.js
@@ -5,9 +5,7 @@ require([
     $(document).ready(function() {
       addField();
 
-      alert($($('.field-row-default').find('input[name="access_date"]')[0]).prop('value'));
-
-//      $($('.field-row-default').find('input[name="access_date"]')[0]).prop('value') = getDate();
+      $($('.field-row-default').find('input[name="access_date"]')[0]).prop('value', getDate());
     });
     $('#add-field-link').on('click', function() {
       addField();
