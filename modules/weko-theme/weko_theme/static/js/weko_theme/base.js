78ead19980c8e941465edde7a0a0d090903a191c
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-05 15:16:09 +0900

    Debug for react-grid-layout.

--- modules/weko-theme/weko_theme/static/js/weko_theme/base.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/base.js
@@ -9,5 +9,9 @@ require([
       $('#btn_edit_stop').on('click', function(){
         window.location.href = '/';
       });
+      $('#harvesting-btn').on('click', function(){
+        alert('harvesting!!!!');
+      });
+
     });
 });
