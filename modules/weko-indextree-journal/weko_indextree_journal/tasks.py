6d1c288bf1341032166b336fd8a24b63ebd2c461
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-02-28 16:39:14 +0700

    [weko3][sp4]Create celery task to export data

--- modules/weko-indextree-journal/weko_indextree_journal/tasks.py
+++ modules/weko-indextree-journal/weko_indextree_journal/tasks.py
@@ -1,30 +1,38 @@
-# -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2016-2018 CERN.
+# This file is part of WEKO3.
+# Copyright (C) 2019 National Institute of Informatics.
 #
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# WEKO3 is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# WEKO3 is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WEKO3; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
 
-"""Celery weko index tree journal tasks."""
+"""Weko Indextree-journal celery tasks."""
 
-from __future__ import absolute_import, print_function
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from invenio_records.models import RecordMetadata
+from invenio_db import db
+from .api import WekoDeposit
+from sqlalchemy.exc import SQLAlchemyError
+from flask import current_app
+from elasticsearch.exceptions import TransportError
 
-from .proxies import current_stats
+logger = get_task_logger(__name__)
 
+#@shared_task(ignore_result=True)
+#@shared_task(base=RequestContextTask)
 @shared_task
-def export_data(data_types):
-    """Index statistics events."""
-    celery_schedule = current_celery.conf.get('CELERY_INDEXTREE_JOURNAL_SCHEDULE', {})
-    print('_______________________CELERY_SCHEDULE____' , celery_schedule)
-    print('_______________________Export_____________' , data_types)
-
-    """
-    results = []
-    for e in data_types:
-        processor = current_stats.events[e].processor_class(
-            **current_stats.events[e].processor_config)
-        results.append((e, processor.run()))
-    return results
-    """
+def export_data():
+    INDEXTREE_JOURNAL_SCHEDULE = celery_schedule = current_app.conf.get('CELERY_INDEXTREE_JOURNAL_SCHEDULE', {})
+    print("-------------------------------------Hello INDEXTREE_JOURNAL!!!-------------------------------------")
