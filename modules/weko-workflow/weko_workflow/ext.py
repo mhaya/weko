147bb70fde12c5b0bdc07891d35e2d30e33d2797
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-03-06 13:58:41 +0900

    Debug for grid design.

--- modules/weko-workflow/weko_workflow/ext.py
+++ modules/weko-workflow/weko_workflow/ext.py
@@ -54,10 +54,10 @@ class WekoWorkflow(object):
         :param app: The Flask application.
         """
         # Use theme's base template if theme is installed
-        if 'BASE_EDIT_TEMPLATE' in app.config:
+        if 'GRID_BASE_PAGE_TEMPLATE' in app.config:
             app.config.setdefault(
                 'WEKO_WORKFLOW_BASE_TEMPLATE',
-                app.config['BASE_PAGE_TEMPLATE'],
+                app.config['GRID_BASE_PAGE_TEMPLATE'],
             )
         for k in dir(config):
             if k.startswith('WEKO_WORKFLOW_'):
